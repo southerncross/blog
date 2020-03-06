5bb2008d79cd7e368801ebd1a7847460a6ba6c97
lishunyang <lishunyang@mokahr.com>
2020-03-07 01:16:57 +0800

    add ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,21 @@ module.exports = {
   title: 'Lishunyang Theme',
   description: "Lishunyang's Blog",
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/assets/favicon.png' }]
+    ['link', { rel: 'icon', type: 'image/png', href: '/assets/favicon.png' }],
+    ['script', { async: true, src='https://www.googletagmanager.com/gtag/js?id=UA-159928407-1' }]
   ],
   themeConfig: {
     logo: '/assets/favicon.png',
     nav: [
       { text: 'Github', link: 'https://github.com/southerncross' },
     ],
-  }
+  },
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-159928407-1'
+      }
+    ]
+  ]
 };
