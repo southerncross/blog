fea0fc8b820ab6468e2b2772dd8b8b5bd3efe383
lishunyang <lishunyang@mokahr.com>
2024-02-26 19:47:30 +0800

    feat: 🔥 Remove vssue and ga plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,19 +14,6 @@ module.exports = {
   base: '/blog/',
   shouldPrefetch: () => false,
   plugins: {
-    '@vuepress/google-analytics': {
-      'ga': 'UA-159928407-1'
-    },
-    '@vssue/vuepress-plugin-vssue': {
-      // set `platform` rather than `api`
-      platform: 'github',
-      locale: 'zh',
-      // all other options of Vssue are allowed
-      owner: 'southerncross',
-      repo: 'blog',
-      clientId: '2797003e5e35203c9d05',
-      clientSecret: 'ec65896b3f509f599200d560eab102eaac984c55',
-    },
     'vuepress-plugin-zooming': {
       selector: '.content img',
       delay: 500,
